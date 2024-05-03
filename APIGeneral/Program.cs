0e6f8fd66e423cdf4c04e06fe2febab8b487767b
Yoennis Pavon Ortega <yoepo2016@gmail.com>
2024-05-03 12:45:22 -0400

    Agregar archivos de proyecto.

---
+++ APIGeneral/Program.cs
@@ -0,0 +1,127 @@
+
+
+using ContratosCore;
+using CumplimientoVentas;
+using Microsoft.Extensions.DependencyInjection;
+using UtilesGenerales;
+var misOrigenesPermitidos = "_misOrigenesPermitidos";
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.WriteIndented = true;
+    options.SerializerOptions.IncludeFields = true;
+
+
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: misOrigenesPermitidos,
+                      policy =>
+                      {
+                          policy.WithOrigins("*").SetIsOriginAllowedToAllowWildcardSubdomains();
+                      });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseCors(misOrigenesPermitidos);
+
+
+Configurador.mapeandoContratos(app);
+Configurador.mapeandoFichasTecnicas(app);
+Configurador.mapeandoCV(app);
+Configurador.mapeandoCPCU(app);
+Configurador.mapeandoTransPortacion(app);
+
+app.Run();
+
+public static class Configurador
+{
+    public static void mapeandoContratos(WebApplication app)
+    {
+        app.MapGet("/ventasporcliente", (string? idCliente) =>
+        {
+            var fechaI = new DateTime(DateTime.Now.Year, 1, 1);
+            var fechaF = DateTime.Now;
+            return OperacionesContratos.ventasPorCliente(fechaI, fechaF, idCliente);
+        });
+
+        app.MapGet("/contratacionporcliente", (string? idCliente) =>
+        {
+            var fechaI = new DateTime(DateTime.Now.Year, 1, 1);
+            var fechaF = DateTime.Now;
+            return OperacionesContratos.resumenContratacion(idCliente);
+        });
+
+    }
+    public static void mapeandoFichasTecnicas(WebApplication app)
+    {
+        app.MapGet("/fichasTecnicas/obtener", (string ft) =>
+        {
+            return FichasTecnicas.obtenerFicheroFichaTecnica(ft);
+        });
+
+        app.MapGet("/fichasTecnicas/obtenerAPartirNombre", (string ft) =>
+        {
+            return FichasTecnicas.obtenerFicheroFichaTecnica(ft, null, true);
+        });
+
+        app.MapGet("/fichasTecnicas/listado", (string rutas) =>
+        {
+            return FichasTecnicas.obtenerListadoFichasTecnicas(rutas: new string[] { rutas });
+        });
+
+    }
+    public static void mapeandoCV(WebApplication app)
+    {
+        app.MapGet("/cv/listado", () =>
+        {
+            return DesgloseFicheros.listado();
+        });
+
+
+        app.MapGet("/aempresarial/listado", (string tipo) =>
+        {
+            return DesgloseFicheros.listado(@"c:/PaginasWeb/AsesorEmpresarial", tipo);
+        });
+    }
+    public static void mapeandoCPCU(WebApplication app)
+    {
+        app.MapGet("/cpcu/empiezapor", (string comienzaPor) =>
+        {
+            return OperacionesCPCU.obtenerCodigosRelacionados(comienzaPor);
+        });
+        app.MapGet("/cpcu/productosCodificados", (string comienzaPor) =>
+         {
+             var retorno = new VMDatosResumenPorFabrica
+             {
+                 codigos = OperacionesCPCU.obtenerProductosCodificados(comienzaPor),
+             };
+
+             return retorno;
+         });
+    }
+    public static void mapeandoTransPortacion(WebApplication app)
+    {
+        app.MapGet("/transportacion/materiasprimas", (DateTime fechaI, DateTime fechaF) =>
+        {
+            return Transportacion.gastosTransportacionMateriaPrima(fechaI,fechaF);
+        });
+
+    }
+    public static void mapeandoFirmasDigitales(WebApplication app)
+    {
+        app.MapGet("/firmasdigitales/cambiarcontrasena", (string ficheroPK12, string contrasenaVieja, string contrasenaNueva) =>
+        {
+            return FirmasDig.FirmasDigitales.cambiarContrasena( ficheroPK12,contrasenaVieja, contrasenaNueva);
+        });
+
+    }
+}
---
+++ FirmasDigitales/FirmasDigitales.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FirmasDig
+{
+    public static class FirmasDigitales
+    {
+        public static string cambiarContrasena(string ficheroPK12, string contrasenaVieja, string contrasenaNueva)
+        {
+            byte[] result = Convert.FromBase64String("FALSO");
+            var datosFichero = Convert.FromBase64String( ficheroPK12 );
+            var caminoFichero = Path.Combine(Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData ),"pk12Actual.p12");
+            // Crear o sobrescribir el archivo binario
+            using (FileStream fileStream = new FileStream(caminoFichero, FileMode.Create))
+            {
+                // Escribir los bytes en el archivo
+                fileStream.Write(datosFichero, 0, datosFichero.Length);
+            }
+
+            try
+            {
+                // Cargar el archivo .p12 con la contraseña actual
+                var certificate = new X509Certificate2(caminoFichero, contrasenaVieja, X509KeyStorageFlags.Exportable);
+                //var ficheroSalida = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                //ficheroSalida = Path.Combine(ficheroSalida, "pk12FicheroCambiado.p12");
+                // Exportar el certificado con la nueva contraseña
+                byte[] exportedData = certificate.Export(X509ContentType.Pkcs12, contrasenaNueva);
+                result = exportedData;
+                // Guardar el certificado exportado con la nueva contraseña
+                //System.IO.File.WriteAllBytes(ficheroSalida, exportedData);
+
+            }
+            catch (Exception ex)
+            {
+            }
+            return Convert.ToBase64String( result );
+        }
+    }
+}
